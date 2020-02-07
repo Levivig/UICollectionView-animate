db488b699ea06ec704bdefd88e3bd234071fba2f
Levente Vig <levivig@gmail.com>
2020-02-07 15:47:23 +0100

    Initial Commit

---
+++ Collection View animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection View animation
+//
+//  Created by Levente Vig on 2020. 02. 07..
+//  Copyright © 2020. levivig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
